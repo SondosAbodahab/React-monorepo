580600fc430dad3e2d62003b6e4478706979fedd
Sondos Abodahab <sondos.ahmed031@gmail.com>
2025-04-04 02:11:16 +0200

    fix: :bug: make inputs controlled

--- apps/travel-list/src/App.js
+++ apps/travel-list/src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  {id: 3 , description: "Charger" , quantity: 1, packed: true}
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -20,10 +22,37 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!description)return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+      initialItems.push(newItem)
+    setDescription("")
+    setQuantity(1)
+
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
-    </div>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -41,8 +70,10 @@ function PackingList() {
 
 function Item({ item }) {
   return (
-    <li>
-      <span style={item.packed ? {textDecoration: "line-through"}: {}} >{item.quantity} {item.description}</span>
+    <li key={item.id}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
       <button>❌</button>
     </li>
   );
