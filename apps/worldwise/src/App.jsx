29825daaacdd9af3e05e9b6d94c254bed3cef020
Sondos Abodahab <sondos.ahmed031@gmail.com>
2025-04-28 20:21:27 +0300

    feat: :sparkles: add initial pages and styling

--- apps/worldwise/src/App.jsx
+++ apps/worldwise/src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import Nav from "./components/Nav";
+import AppLayout from './pages/AppLayout'
 
 function App() {
-  
-
   return (
-    <div>worldwise</div>
-  )
+    <div>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ apps/worldwise/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+
+function HomePage() {
+  return (
+    <div>
+      <NavLink to='/app'>Home</NavLink>
+    </div>
+  );
+}
+
+export default HomePage;
