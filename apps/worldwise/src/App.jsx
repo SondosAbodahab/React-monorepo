ae05b1f9e033252017b5b8ccecd1b2593cbd88da
Sondos Abodahab <sondos.ahmed031@gmail.com>
2025-04-29 23:14:00 +0300

    feat: :sparkles: add the list list

--- apps/worldwise/src/App.jsx
+++ apps/worldwise/src/App.jsx
@@ -7,8 +7,31 @@ import NotFound from "./pages/NotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const Base_URL = "http://localhost:3000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${Base_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data.");
+      } finally{
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -18,8 +41,8 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-          <Route path="cities" element={<CityList />}/>
-            <Route path="cities" element={<CityList />} />
+            <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
+            <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
             <Route path="countries" element={<p>Countries</p>}></Route>
             <Route path="form" element={<p>Form</p>}></Route>
           </Route>
--- apps/worldwise/src/components/CityList.jsx
+++ apps/worldwise/src/components/CityList.jsx
@@ -1,11 +1,18 @@
-import styles from './CityList.module.css'
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Massage from './Message'
 
-function CityList() {
-    return (
-        <ul className={styles.cityList}>
-            list
-        </ul>
-    )
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if(!cities.length) return <Massage massage='Add your first city by clicking on a city on the map'/>;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem key={city.cityName} city={city} />
+      ))}
+    </ul>
+  );
 }
 
-export default CityList
+export default CityList;
