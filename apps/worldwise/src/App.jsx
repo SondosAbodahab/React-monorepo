2d53045f9ccd162fce7b2368eb2fbd001807e802
Sondos Abodahab <sondos.ahmed031@gmail.com>
2025-04-29 20:10:09 +0300

    feat: :sparkles: add json-server and nested routes for cities & countries

--- apps/worldwise/src/App.jsx
+++ apps/worldwise/src/App.jsx
@@ -4,8 +4,9 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
-import AppLayout from './pages/AppLayout';
-import Login from './pages/Login'
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
@@ -16,7 +17,12 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<CityList />}/>
+            <Route path="cities" element={<CityList />} />
+            <Route path="countries" element={<p>Countries</p>}></Route>
+            <Route path="form" element={<p>Form</p>}></Route>
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ apps/worldwise/src/components/CityList.jsx
@@ -0,0 +1,11 @@
+import styles from './CityList.module.css'
+
+function CityList() {
+    return (
+        <ul className={styles.cityList}>
+            list
+        </ul>
+    )
+}
+
+export default CityList
